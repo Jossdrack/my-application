ad629d5b9b0a52957926dced418214cb502a07ad
Josué Drack <joss.drack@gmail.com>
2019-03-16 16:05:29 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyCompComponent } from './comp/my-comp/my-comp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyCompComponent
   ],
   imports: [
     BrowserModule,
